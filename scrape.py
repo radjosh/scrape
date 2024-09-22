bb49d0d9ee87eddf91e504642c3553ce25613518
Joshua M Smith <radjosh@gmail.com>
2024-09-22 12:34:13 -0400

    added requests module

--- scrape.py
+++ scrape.py
@@ -1,3 +1,9 @@
 from bs4 import BeautifulSoup
 import selenium
 import scrapy
+import requests
+
+TEST_URL = 'https://www.scrapingcourse.com/ecommerce/'
+
+response = requests.get(TEST_URL)
+print(response)
