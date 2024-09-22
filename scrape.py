bb4d8da3421625b5afe77c7fe147094001c8c658
Joshua M Smith <radjosh@gmail.com>
2024-09-22 12:37:55 -0400

    basic bs4 scraping

--- scrape.py
+++ scrape.py
@@ -6,4 +6,11 @@ import requests
 TEST_URL = 'https://www.scrapingcourse.com/ecommerce/'
 
 response = requests.get(TEST_URL)
-print(response)
+# print(response)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+product_names = soup.find_all("h2")
+
+for product_name in product_names:
+    print(product_name.text)
