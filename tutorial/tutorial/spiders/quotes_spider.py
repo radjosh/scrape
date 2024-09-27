57fd04bcdc4b5dfcf3e39fbc4924200fd7370719
Joshua M Smith <radjosh@gmail.com>
2024-09-27 09:45:53 -0400

    tracking tutorial quotes spider

---
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import scrapy
+
+class QuoteSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        "https://quotes.toscrape.com/page/1/",
+        "https://quotes.toscrape.com/page/2/",
+    ]
+
+    #def start_requests(self):
+        #urls = [
+            #"https://quotes.toscrape.com/page/1/",
+            #"https://quotes.toscrape.com/page/2/",
+        #]
+        #for url in urls:
+            #yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f"quotes-{page}.html"
+        Path(filename).write_bytes(response.body)
+        #self.log(f"Saved file {filename}")
